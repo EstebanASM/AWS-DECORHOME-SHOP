a41e16bc42fad438c8cd174851c8f737d01bd8fc
EstebanASM <150808482+EstebanASM@users.noreply.github.com>
2025-02-01 23:56:02 -0500

    feat: :sparkles: Dockerization and database initialization for the stock microservice

--- Backend/DataBases/stock/init-mongo.sh
+++ Backend/DataBases/stock/init-mongo.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Esperar a que MongoDB esté listo
+# Configurar variables
+MONGO_HOST="localhost"
+
 echo "Esperando a que MongoDB esté listo..."
-until mongo --host $MONGO_HOST --eval "print('Esperando a MongoDB')" &>/dev/null; do
+until mongosh --host $MONGO_HOST --eval "print('MongoDB está listo')" &>/dev/null; do
   sleep 1
 done
 
 # Importar datos de ejemplo en la colección "stock"
-echo "Importando datos..."
+echo "Importando datos en la base de datos db_stock..."
 mongoimport --host $MONGO_HOST --db db_stock --collection stock --type json --file /data/stock.json --jsonArray
 
-echo "Datos importados correctamente"
+echo "Datos importados correctamente."
