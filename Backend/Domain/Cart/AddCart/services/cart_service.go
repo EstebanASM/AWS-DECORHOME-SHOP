19642bad68f50c105d3f44413c5f645db2393fa5
EstebanASM <150808482+EstebanASM@users.noreply.github.com>
2025-02-08 20:07:10 -0500

    feat: :sparkles: Creation of the shopping cart microservice

---
+++ Backend/Domain/Cart/AddCart/services/cart_service.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"Backend/Domain/Cart/AddCart/config"
+	"Backend/Domain/Cart/AddCart/handlers"
+)
+
+func main() {
+	// Inicializar la configuración y conexiones a las bases de datos.
+	if err := config.Init(); err != nil {
+		log.Fatalf("Error en la inicialización: %v", err)
+	}
+
+	// Configurar la ruta para el WebSocket.
+	http.HandleFunc("/ws", handlers.HandleWebSocket)
+
+	port := 8080
+	log.Printf("Servidor WebSocket escuchando en :%d", port)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Error en ListenAndServe: %v", err)
+	}
+}
