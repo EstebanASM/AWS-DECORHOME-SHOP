31fb27fbaed5eba71b2ba29d6194c090653485af
EstebanASM <150808482+EstebanASM@users.noreply.github.com>
2025-02-10 14:22:06 -0500

    feat: :sparkles: Modification of models in my get cart microservice

--- Backend/Domain/Cart/GetCarts/models/cart.go
+++ Backend/Domain/Cart/GetCarts/models/cart.go
@@ -1,10 +1,9 @@
 package models
 
 type CartItem struct {
-	ID          int    `json:"id"`
-	ProductID   string `json:"product_id"` // ID del producto, se maneja como string (se pasa de MongoDB a MySQL)
-	Quantity    int    `json:"quantity"`
-	AddedAt     string `json:"added_at"` // Convertido a string para evitar error de conversión
-	UpdatedAt   string `json:"updated_at"`
-	ProductName string `json:"product_name"` // Nuevo campo para el nombre del producto
+	ID        int    `json:"id"`
+	ProductID string `json:"product_id"` // ID del producto, se maneja como string (se pasa de MongoDB a MySQL)
+	Quantity  int    `json:"quantity"`
+	AddedAt   string `json:"added_at"` // Convertido a string para evitar error de conversión
+	UpdatedAt string `json:"updated_at"`
 }
