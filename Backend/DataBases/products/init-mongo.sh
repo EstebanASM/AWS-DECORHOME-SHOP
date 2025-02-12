6966efa4621f613a50b138a06412914cc4fbd810
EstebanASM <150808482+EstebanASM@users.noreply.github.com>
2025-02-12 17:49:32 -0500

    feat: :sparkles: Modification of the mongo.yml file

--- Backend/DataBases/products/init-mongo.sh
+++ Backend/DataBases/products/init-mongo.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+echo "⏳ Esperando a que MongoDB inicie..."
+sleep 10
+
+# Importar los datos a MongoDB
+echo "📦 Importando datos en db_products..."
 mongoimport --host localhost --db db_products --collection products --type json --file /data/products.json --jsonArray
+
+echo "✅ Datos importados correctamente."
