4b545d34dc79ac99379cff9c47dea3e9b6fb612b
EstebanASM <150808482+EstebanASM@users.noreply.github.com>
2025-02-13 17:03:59 -0500

    feat: :sparkles: Creating and modifying the yml

--- Backend/Domain/Category/GetCategory/src/database.py
+++ Backend/Domain/Category/GetCategory/src/database.py
@@ -3,9 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL de conexión de la base de datos PostgreSQL desde la variable de entorno
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:ksdkosndk1212..@categorydb.cvzr7pomd22b.us-east-1.rds.amazonaws.com:5432/categorydb")
+environment = os.getenv("ENVIRONMENT", "development")  # Por defecto será "development"
 
+if environment == "production":
+    # URL de conexión a la base de datos en AWS RDS (para producción)
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:ksdkosndk1212..@categorydb.cvzr7pomd22b.us-east-1.rds.amazonaws.com:5432/categorydb")
+else:
+    # URL de conexión a la base de datos en localhost (para desarrollo)
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:1753791003@localhost:5432/db_categories")
+    
 # Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL)
 
--- Backend/Domain/Category/GetCategoryId/src/database.py
+++ Backend/Domain/Category/GetCategoryId/src/database.py
@@ -3,9 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# URL de conexión de la base de datos PostgreSQL desde la variable de entorno
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:ksdkosndk1212..@categorydb.cvzr7pomd22b.us-east-1.rds.amazonaws.com:5432/categorydb")
+environment = os.getenv("ENVIRONMENT", "development")  # Por defecto será "development"
 
+if environment == "production":
+    
+    # URL de conexión a la base de datos en AWS RDS (para producción)
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:ksdkosndk1212..@categorydb.cvzr7pomd22b.us-east-1.rds.amazonaws.com:5432/categorydb")
+else:
+    # URL de conexión a la base de datos en localhost (para desarrollo)
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:1753791003@localhost:5432/db_categories")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
