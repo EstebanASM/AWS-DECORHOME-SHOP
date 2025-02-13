5912e7c7d0ec99c6f53479b5ce1c714369f704d2
EstebanASM <150808482+EstebanASM@users.noreply.github.com>
2025-02-12 19:21:09 -0500

    feat: :sparkles: Changes in yml and docker file

--- Backend/DataBases/products/init-mongo.sh
+++ Backend/DataBases/products/init-mongo.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-echo "⏳ Esperando a que MongoDB inicie..."
-sleep 10
-
-echo "📦 Creando usuario administrador en MongoDB..."
-mongo admin --eval "
-  db.createUser({
-    user: '${MONGO_INITDB_ROOT_USERNAME}',
-    pwd: '${MONGO_INITDB_ROOT_PASSWORD}',
-    roles: [{ role: 'root', db: 'admin' }]
-  });
-"
-
-echo "📂 Importando datos en db_products..."
 mongoimport --host localhost --db db_products --collection products --type json --file /data/products.json --jsonArray
-
-echo "✅ Datos importados correctamente."
