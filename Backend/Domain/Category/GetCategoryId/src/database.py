55dec5cced5e60060dad2e477230ee6d4e18b29e
EstebanASM <150808482+EstebanASM@users.noreply.github.com>
2025-02-13 09:16:42 -0500

    feat: :sparkles: Modification of the yml and changes in the code

--- Backend/Domain/Category/GetCategoryId/src/database.py
+++ Backend/Domain/Category/GetCategoryId/src/database.py
@@ -3,8 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# URL de conexión a la base de datos
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:1753791003@localhost:5432/db_categories")
+# URL de conexión de la base de datos PostgreSQL desde la variable de entorno
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:ksdkosndk1212..@categorydb.cvzr7pomd22b.us-east-1.rds.amazonaws.com:5432/categorydb")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
